26136c48692ca04f2b7888c00f889b4e2b867fb0
Dominic Wöhrle <dominic.woehrle@gmail.com>
2024-06-24 22:54:41 +0200

    improve log saving and rename class

--- chat.py
+++ chat.py
@@ -1,15 +1,14 @@
-import os
-import json
 from loguru import logger
 from openai import OpenAI
 
 
-class Chat:
+class RomanticChat:
     def __init__(self, client: OpenAI, own_name: str, partner_name: str, model: str = "gpt-3.5-turbo") -> None:
-        self.client = client
-        self.model = model
         self.own_name = own_name
         self.partner_name = partner_name
+        
+        self.client = client
+        self.model = model
         self.total_tokens_used = 0
         self.messages = [
             {
@@ -22,7 +21,6 @@ class Chat:
         """ Sends a message to the partner
         Args:
             message (str): The message to be sent to the partner.
-        
         Returns:
             str: The response from the partner.
         """
@@ -55,30 +53,22 @@ class Chat:
             logger.error("Request to openai failed due to:\n" + str(e))
             return "An error has been occured while making an openai request!"
 
-    # TODO The log is not valid json, please don't use it for automated things
     def save_log(self) -> None:
-        """ Appends the message history and the total tokens used to a json file. Creates the file if not exists.
+        """ Appends the message history and the total tokens used to a log file. Creates the file if not exists.
         Args:
             None
-
         Returns:
             None
         """
-        log_path = "log.json"
+        log_path = "log.txt"
 
-        # Create file if not exists
-        if not os.path.exists(log_path):
-            with open(log_path, "w", encoding="utf-8") as file:
-                file.write("")
-        
-        # Append log file with new data
-        with open(log_path, "a", encoding="utf-8") as file:
-            if not file.writable():
-                logger.error("Logging file is not writable!")
-                return
-            
-            log_dict = {
-                "messages": self.messages,
-                "total_tokens_used": self.total_tokens_used
-            }
-            file.write(json.dumps(log_dict, ensure_ascii=False) + "\n")
+        request_amount = int((len(self.messages) - 1) / 2)
+
+        output_str = ""
+        output_str += f"Chat between own_name '{self.own_name}' and partner_name '{self.partner_name}'\n"
+        output_str += f"Used model: {self.model}\n"
+        output_str += f"Amount of requests: {request_amount}\n"
+        output_str += f"Total tokens used: {self.total_tokens_used}\n"
+
+        with open(log_path, "w", encoding="utf-8") as file:
+            file.write(output_str)
